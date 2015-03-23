6cc8fbf690e5b3dcae882ba4ef28c79987d3b1aa
Mason Moreland <mason@192.168.1.107>
2015-03-23 01:29:16 -0500

    Initial Setup

---
+++ Wizard-Treasure-Chase/src/FixedObject/FixedObject.java
@@ -0,0 +1,22 @@
+/*
+ * This is an assigned group project for CSE1325
+ * Members: Mason Moreland, Ryan Rogers, Raith Hamhaz
+ */
+package FixedObject;
+
+/**
+ *
+ * @author mason
+ */
+public class FixedObject
+{
+    protected int longitude;
+    
+    protected int latitude;
+
+    public FixedObject()
+    {
+        longitude = -1;
+        latitude = -1;
+    }
+}
